c5b1be6b53265835fa785d5b6609fe9aee48f534
fagumus <fa.gumus@gmail.com>
2022-06-29 13:04:28 +0300

    Add files via upload

---
+++ DAYS_OF_PYTHON/day3/script/problem1.py
@@ -0,0 +1,18 @@
+# Describe Problem
+"""def my_function():
+  for i in range(1, 20):
+    if i == 20:
+      print("You got it")
+my_function()"""
+
+
+def my_function():
+    """ Problem solved """
+    for i in range(1, 21):
+        print("i = ", i)
+        if i == 20:
+            print("You got it")
+
+
+my_function()
+
